600bd629467b5161a47f8c717f3f4398092eee35
Enzo <enzo.erkmann5@gmail.com>
2026-06-06 13:54:26 -0300

    feat/ Added archives to make conections between repo and snowflake by terraform

--- infra/terraform/providers.tf
+++ infra/terraform/providers.tf
@@ -1,8 +1,14 @@
+# Arquivo: infra/terraform/providers.tf
+
 terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
       version = "~> 5.0"
+    }
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "~> 0.87"
     }
   }
 }
@@ -10,4 +16,13 @@ terraform {
 provider "google" {
   project = var.project_id
   region  = var.region
+}
+
+# Modificação aqui: Agora passamos os argumentos via variáveis controladas do TF
+provider "snowflake" {
+  user              = var.snowflake_user
+  password          = var.snowflake_password
+  account_name      = var.snowflake_account_name
+  organization_name = var.snowflake_organization_name
+  role              = "ACCOUNTADMIN"
 }
--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -8,3 +8,24 @@ variable "region" {
   type        = string
   description = "Região principal dos recursos"
 }
+
+variable "snowflake_user" {
+  type        = string
+  description = "Usuário do Snowflake"
+}
+
+variable "snowflake_password" {
+  type        = string
+  description = "Senha do Snowflake"
+  sensitive   = true
+}
+
+variable "snowflake_organization_name" {
+  type        = string
+  description = "Nome da organização no Snowflake"
+}
+
+variable "snowflake_account_name" {
+  type        = string
+  description = "Nome da conta no Snowflake"
+}
